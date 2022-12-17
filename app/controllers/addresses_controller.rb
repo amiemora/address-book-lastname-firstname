3a901963502ca18f5a87c2a1b1c1b7cb5887bcd9
Amie Morales <xamiemmorales@gmail.com>
2022-12-16 20:37:42 -0600

    Address controller check

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,25 +1,77 @@
 class AddressesController < ApplicationController
   def index
+    @addresses = Address.all
   end
 
   def show
+    @address = Address.find(params[:id])
   end
 
   def new
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+    respond_to do |format|
+      if @address.save
+        format.html do
+          redirect_to @address, notice: "Address was successfully created"
+        end
+        format.json { render :show, status: :created, location: @address }
+      else
+        format.html { render :new }
+        format.json do
+          render json: @address.errors, status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    respond_to do |format|
+      if @address.update(address_params)
+        format.html do
+          redirect_to @address, notice: "address was successfully updated"
+        end
+        format.json { render :show, status: :created, location: @address }
+      else
+        format.html { render :edit }
+        format.json do
+          render json: @address.errors, status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   def delete
+    @address = Address.find(params[:id])
   end
 
   def destroy
+    @address.destroy
+    respond_to do |format|
+      format.html do
+        redirect_to addresses_url, notice: "person was successfully deleted"
+      end
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(
+      :person_id,
+      :street,
+      :town,
+      :zip,
+      :state,
+      :country
+    )
   end
 end
