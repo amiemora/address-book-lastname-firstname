b342a6da3541ed94505ad2a19095696e8ea70d16
Amie Morales <xamiemmorales@gmail.com>
2022-12-20 14:13:04 -0600

    fixed root page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
   def index
+    @people = current_user.people.all
+    @user = current_user
+    @addresses = current_user.addresses.all
+    @email = current_user.emails.all
+    @phone_number = current_user.phone_numbers.all
   end
 end
